fcd8b1348316aa6e97103d718e74acda30312667
Rico Apon <Rico.Apon@keylane.com>
2021-12-02 10:26:44 +0100

    Implement day 2

---
+++ year-2021/src/main/java/nl/ricoapon/day2/Position.java
@@ -0,0 +1,24 @@
+package nl.ricoapon.day2;
+
+public class Position {
+    private int depth = 0;
+    private int horizontalPosition = 0;
+
+    public void apply(Command command) {
+        if (command.getDirection() == Command.Direction.UP) {
+            depth -= command.getX();
+        } else if (command.getDirection() == Command.Direction.DOWN) {
+            depth += command.getX();
+        } else {
+            horizontalPosition += command.getX();
+        }
+    }
+
+    public int getDepth() {
+        return depth;
+    }
+
+    public int getHorizontalPosition() {
+        return horizontalPosition;
+    }
+}
