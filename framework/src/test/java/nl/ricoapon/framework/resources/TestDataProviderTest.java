0aa7907a0c4952998b0777d0aebd3661de47a144
Rico Apon <Rico.Apon@keylane.com>
2021-12-03 08:34:38 +0100

    Fix tests that did not compile

--- framework/src/test/java/nl/ricoapon/framework/resources/TestDataProviderTest.java
+++ framework/src/test/java/nl/ricoapon/framework/resources/TestDataProviderTest.java
@@ -12,17 +12,17 @@ class TestDataProviderTest {
     void day1IsCorrectlySortedAndCreated() {
         List<TestData> testData = TestDataProvider.determineTestDataForDay(1);
         assertEquals(5, testData.size());
-        assertEquals(new TestData(1, 1, "input", "?"), testData.get(0));
-        assertEquals(new TestData(1, 1, "part1_example1", "1"), testData.get(1));
-        assertEquals(new TestData(1, 1, "part1_example2", "2"), testData.get(2));
-        assertEquals(new TestData(1, 2, "input", "?"), testData.get(3));
-        assertEquals(new TestData(1, 2, "part2_example1", "3"), testData.get(4));
+        assertEquals(new TestData(1, 1, "input", "?", false, null), testData.get(0));
+        assertEquals(new TestData(1, 1, "part1_example1", "1", true, 1), testData.get(1));
+        assertEquals(new TestData(1, 1, "part1_example2", "2", true, 2), testData.get(2));
+        assertEquals(new TestData(1, 2, "input", "?", false, null), testData.get(3));
+        assertEquals(new TestData(1, 2, "part2_example1", "3", true, 1), testData.get(4));
     }
 
     @Test
     void singeFileIsAllowed() {
         List<TestData> testData = TestDataProvider.determineTestDataForDay(2);
         assertEquals(1, testData.size());
-        assertEquals(new TestData(2, 1, "input", "?"), testData.get(0));
+        assertEquals(new TestData(2, 1, "input", "?", false, null), testData.get(0));
     }
 }
