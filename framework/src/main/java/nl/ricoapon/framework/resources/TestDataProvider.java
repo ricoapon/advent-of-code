4358313aeda6b413b31a3c448494a1a9a1e41681
Rico Apon <rico.apon@gmail.com>
2021-12-16 10:35:43 +0100

    Read test data from YAML file

--- framework/src/main/java/nl/ricoapon/framework/resources/TestDataProvider.java
+++ framework/src/main/java/nl/ricoapon/framework/resources/TestDataProvider.java
@@ -1,54 +1,53 @@
 package nl.ricoapon.framework.resources;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 public class TestDataProvider {
+
+    private static Expected expectedOfDay(int day) {
+        String content = FileUtil.readContentFromResource(getPath(day, "expected.yml"));
+        return Expected.of(content);
+    }
+
     /**
      * @param day The day of which the test data is created.
      * @return A list of all test data that can be used for running tests.
      */
     public static List<TestData> determineTestDataForDay(int day) {
+        Expected expected = expectedOfDay(day);
+
         List<TestData> testData = new ArrayList<>();
-        List<InputFile> inputFiles = InputFileProvider.getAllInputFiles(day);
-        if (inputFiles.size() == 0) {
-            return testData;
-        }
-
-        List<String> allExpectedAnswers;
-        try {
-            allExpectedAnswers = Arrays.stream(FileUtil.readContentFromResource("/day" + day + "/expected.txt").split("\n")).toList();
-        } catch (RuntimeException e) {
-            // No expected list could be found. So we just continue with the next day.
-            return testData;
-        }
-
-        List<InputFile> inputFilesForPart1 = inputFiles.stream()
-                .filter(InputFile::canBeUsedForPart1)
-                .toList();
-        List<String> expectedAnswersPart1 = allExpectedAnswers.subList(0, Math.min(inputFilesForPart1.size(), allExpectedAnswers.size()));
-        testData.addAll(createTestDataForSinglePart(day, 1, inputFilesForPart1, expectedAnswersPart1));
-
-        List<InputFile> inputFilesForPart2 = inputFiles.stream()
-                .filter(InputFile::canBeUsedForPart2)
-                .toList();
-        List<String> expectedAnswersPart2 = allExpectedAnswers.subList(expectedAnswersPart1.size(),
-                expectedAnswersPart1.size() + Math.min(inputFilesForPart2.size(), allExpectedAnswers.size() - expectedAnswersPart1.size()));
-        testData.addAll(createTestDataForSinglePart(day, 2, inputFilesForPart2, expectedAnswersPart2));
+        testData.addAll(createTestDataForSinglePart(day, 1, expected.part1));
+        testData.addAll(createTestDataForSinglePart(day, 2, expected.part2));
 
         return testData;
     }
 
-    private static List<TestData> createTestDataForSinglePart(int day, int part, List<InputFile> inputFiles, List<String> expectedAnswers) {
+    private static List<TestData> createTestDataForSinglePart(int day, int part, Map<String, String> filesToExpectedResultMap) {
         List<TestData> testData = new ArrayList<>();
 
-        for (int i = 0; i < Math.min(inputFiles.size(), expectedAnswers.size()); i++) {
-            InputFile inputFile = inputFiles.get(i);
-            String expected = expectedAnswers.get(i);
-            testData.add(new TestData(day, part, inputFile.readContent(), expected, inputFile.isExample()));
+        for (Map.Entry<String, String> entry : filesToExpectedResultMap.entrySet()) {
+            String expected = entry.getValue();
+            String filename = entry.getKey().endsWith(".txt") ? entry.getKey() : entry.getKey() + ".txt";
+
+            testData.add(new TestData(day,
+                    part,
+                    FileUtil.readContentFromResource(getPath(day, filename)),
+                    expected,
+                    isExample(filename)));
         }
 
         return testData;
+    }
+
+    private static String getPath(int day, String filename) {
+        return "/day" + day + "/" + filename;
+    }
+
+    private static boolean isExample(String filename) {
+        return !"input.txt".equals(filename);
     }
 }
+
