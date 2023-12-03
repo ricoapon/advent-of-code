551d440ef5900c468883274fda9946909cd6e287
Rico Apon <rico.apon@gmail.com>
2023-12-03 09:17:24 +0000

    Implement year 2023 day 3

--- utilities/src/main/java/nl/ricoapon/Coordinate2D.java
+++ utilities/src/main/java/nl/ricoapon/Coordinate2D.java
@@ -1,4 +1,52 @@
 package nl.ricoapon;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 public record Coordinate2D(int x, int y) {
+    public Collection<Coordinate2D> getHorizontalAndVerticalAdjacent(int maxX, int maxY) {
+        List<Coordinate2D> adjacent = new ArrayList<>();
+        if (x > 0) {
+            adjacent.add(new Coordinate2D(x - 1, y));
+        }
+        if (y > 0) {
+            adjacent.add(new Coordinate2D(x, y - 1));
+        }
+        if (x < maxX) {
+            adjacent.add(new Coordinate2D(x + 1, y));
+        }
+        if (y < maxY) {
+            adjacent.add(new Coordinate2D(x, y + 1));
+        }
+        return adjacent;
+    }
+
+    public Collection<Coordinate2D> getDiagonalAdjacent(int maxX, int maxY) {
+        List<Coordinate2D> adjacent = new ArrayList<>();
+        // Top left
+        if (x > 0 && y > 0) {
+            adjacent.add(new Coordinate2D(x - 1, y - 1));
+        }
+        // Top right.
+        if (x > 0 && y < maxY) {
+            adjacent.add(new Coordinate2D(x - 1, y + 1));
+        }
+        // Bottom left.
+        if (x < maxX && y > 0) {
+            adjacent.add(new Coordinate2D(x + 1, y - 1));
+        }
+        // Bottom right.
+        if (x < maxX && y < maxY) {
+            adjacent.add(new Coordinate2D(x + 1, y + 1));
+        }
+        return adjacent;
+    }
+
+    public Collection<Coordinate2D> getAdjacent(int maxX, int maxY) {
+        List<Coordinate2D> adjacent = new ArrayList<>();
+        adjacent.addAll(getHorizontalAndVerticalAdjacent(maxX, maxY));
+        adjacent.addAll(getDiagonalAdjacent(maxX, maxY));
+        return adjacent;
+    }
 }
---
+++ year-2023/src/main/java/nl/ricoapon/day3/SchematicNumber.java
@@ -0,0 +1,9 @@
+package nl.ricoapon.day3;
+
+import java.util.List;
+
+import nl.ricoapon.Coordinate2D;
+
+public record SchematicNumber(Integer value, List<Coordinate2D> coordinates) {
+    
+}
