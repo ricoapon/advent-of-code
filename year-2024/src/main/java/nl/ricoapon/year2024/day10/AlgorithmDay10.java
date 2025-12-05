58b441a53524716e2cf8c55f7137d7e401afed4f
Rico Apon <rico.apon@gmail.com>
2025-12-05 12:04:46 +0000

    Remove unused import

--- year-2024/src/main/java/nl/ricoapon/year2024/day10/AlgorithmDay10.java
+++ year-2024/src/main/java/nl/ricoapon/year2024/day10/AlgorithmDay10.java
@@ -6,7 +6,6 @@ import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
-import java.util.Set;
 
 import nl.ricoapon.Grid;
 import nl.ricoapon.framework.Algorithm;
