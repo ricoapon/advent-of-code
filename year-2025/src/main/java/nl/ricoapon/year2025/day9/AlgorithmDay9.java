4dfd2868f817ef84da59f871a7ad5862a5da7583
Rico Apon <rico.apon@gmail.com>
2025-12-10 07:57:09 +0000

    Try day 9 part 2

--- year-2025/src/main/java/nl/ricoapon/year2025/day9/AlgorithmDay9.java
+++ year-2025/src/main/java/nl/ricoapon/year2025/day9/AlgorithmDay9.java
@@ -1,5 +1,6 @@
 package nl.ricoapon.year2025.day9;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -37,18 +38,19 @@ public class AlgorithmDay9 implements Algorithm {
         VERTICAL;
     }
 
-    // Only horizontal lines can go left or right and only vertical lines can go up
-    // or down.
-    protected enum Direction {
-        LEFT,
-        RIGHT,
-        UP,
-        DOWN;
-    }
-
-    protected record Line(Coordinate2D a, Coordinate2D b, Axis axis, Direction direction) {
+    protected record Line(Coordinate2D a, Coordinate2D b, Axis axis) {
         public Line(Coordinate2D a, Coordinate2D b) {
-            this(a, b, determineAxis(a, b), determineDirection(a, b));
+            this(smallest(a, b), largest(a, b), determineAxis(a, b));
+        }
+
+        private static Coordinate2D smallest(Coordinate2D a, Coordinate2D b) {
+            // Since we know either x or y is equal, this will return either a or b.
+            return new Coordinate2D(Math.min(a.x(), b.x()), Math.min(a.y(), b.y()));
+        }
+
+        private static Coordinate2D largest(Coordinate2D a, Coordinate2D b) {
+            // Since we know either x or y is equal, this will return either a or b.
+            return new Coordinate2D(Math.max(a.x(), b.x()), Math.max(a.y(), b.y()));
         }
 
         private static Axis determineAxis(Coordinate2D a, Coordinate2D b) {
@@ -59,56 +61,80 @@ public class AlgorithmDay9 implements Algorithm {
             return Axis.HORIZONTAL;
         }
 
-        private static Direction determineDirection(Coordinate2D a, Coordinate2D b) {
-            if (determineAxis(a, b) == Axis.VERTICAL) {
-                if (a.y() < b.y()) {
-                    return Direction.UP;
-                }
-                return Direction.DOWN;
+        // We exclude the start and end points, because obviously they are on a line.
+        public boolean hasSinglePointInsideLineIntersectionWith(Line other) {
+            // We can only have a single point if have a vertical and horizontal line.
+            if (axis == other.axis) {
+                return false;
             }
-            if (a.x() < b.x()) {
-                return Direction.RIGHT;
+
+            // If a point exists, it must be this one. If it is also on both lines, we
+            // found that single point.
+            var x = axis == Axis.VERTICAL ? a.x() : other.a.x();
+            var y = axis == Axis.HORIZONTAL ? a.y() : other.a.y();
+            var c = new Coordinate2D(x, y);
+
+            if (c.equals(a) || c.equals(b) || c.equals(other.a) || c.equals(other.b)) {
+                return false;
             }
-            return Direction.LEFT;
-        }
 
-        public int minX() {
-            return Math.min(a.x(), b.x());
-        }
-
-        public int maxX() {
-            return Math.max(a.x(), b.x());
-        }
-
-        public int minY() {
-            return Math.min(a.y(), b.y());
-        }
-
-        public int maxY() {
-            return Math.max(a.y(), b.y());
-        }
-
-        public boolean canWalkOutToLine(Line other) {
-            return switch (direction) {
-                case UP -> other.a.y() > a.y();
-                case DOWN -> other.a.y() < a.y();
-                case RIGHT -> other.a.x() < a.x();
-                case LEFT -> other.a.x() > a.x();
-            };
+            return contains(c) && other.contains(c);
         }
 
         public boolean contains(Coordinate2D c) {
-            return switch (direction) {
-                case UP -> c.x() == a.x() && a.y() <= c.y() && c.y() <= b.y();
-                case DOWN -> c.x() == a.x() && a.y() >= c.y() && c.y() >= b.y();
-                case RIGHT -> c.y() == a.y() && a.x() >= c.x() && c.x() >= b.x();
-                case LEFT -> c.y() == a.y() && a.x() <= c.x() && c.x() <= b.x();
+            return switch (axis) {
+                case VERTICAL -> c.x() == a.x() && a.y() <= c.y() && c.y() <= b.y();
+                case HORIZONTAL -> c.y() == a.y() && a.x() <= c.x() && c.x() <= b.x();
             };
         }
     }
 
     @Override
     public Object part2(String input) {
-        return "x";
+        List<Coordinate2D> coordinates = Stream.of(input.split("\\r?\\n")).map(this::of).toList();
+        List<Line> lines = new ArrayList<>();
+        for (int i = 0; i < coordinates.size(); i++) {
+            var a = coordinates.get(i);
+            var b = (i + 1 < coordinates.size()) ? coordinates.get(i + 1) : coordinates.get(0);
+            lines.add(new Line(a, b));
+        }
+
+        long biggestSize = 0;
+        for (Coordinate2D a : coordinates) {
+            for (Coordinate2D b : coordinates) {
+                if (a.equals(b) || a.x() == b.x() || a.y() == b.y()) {
+                    continue;
+                }
+                var newSquareSize = squareSize(a, b);
+                if (newSquareSize > biggestSize && isSquareInsideBorder(lines, a, b)) {
+                    biggestSize = newSquareSize;
+                }
+            }
+        }
+
+        return biggestSize;
+    }
+
+    private boolean isSquareInsideBorder(List<Line> lines, Coordinate2D a, Coordinate2D b) {
+        // This uses a trick that no two lines inside the list of lines is colinear.
+        // We can reduce the check to if there is a single point intersection.
+        List<Line> squareLines = List.of(
+                new Line(a, new Coordinate2D(a.x(), b.y())),
+                new Line(a, new Coordinate2D(b.x(), a.y())),
+                new Line(new Coordinate2D(a.x(), b.y()), b),
+                new Line(new Coordinate2D(b.x(), a.y()), b));
+
+        for (Line squareLine : squareLines) {
+            if (lines.stream().filter(squareLine::hasSinglePointInsideLineIntersectionWith).count() > 0) {
+                return false;
+            }
+        }
+
+        // TODO: find out a way to exclude this case:
+        // ..O.
+        // .OX.
+        // Where O are the corners. All four corners must be inside!
+
+        return true;
     }
 }
--- year-2025/src/test/java/nl/ricoapon/year2025/day9/AlgorithmDay9Test.java
+++ year-2025/src/test/java/nl/ricoapon/year2025/day9/AlgorithmDay9Test.java
@@ -1,30 +1,22 @@
 package nl.ricoapon.year2025.day9;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.junit.jupiter.api.Test;
+
 import nl.ricoapon.Coordinate2D;
 import nl.ricoapon.framework.testrunner.AlgorithmDayTestRunnerUtil;
 import nl.ricoapon.year2025.day9.AlgorithmDay9.Line;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.util.List;
-
-import org.junit.jupiter.api.Test;
 
 class AlgorithmDay9Test {
     private final AlgorithmDayTestRunnerUtil algorithmDayTestRunnerUtil = new AlgorithmDayTestRunnerUtil(2025, 9);
 
     @Test
-    void lineContainsWorks() {
-        var a = new Coordinate2D(1,3);
-        var middle = new Coordinate2D(1, 4);
-        var b = new Coordinate2D(1, 6);
-        var line1 = new Line(a, b);
-        var line2 = new Line(b, a);
+    void smallTest() {
+        Line squareLine = new Line(new Coordinate2D(9, 3), new Coordinate2D(9, 7));
+        Line edgeLine = new Line(new Coordinate2D(9, 7), new Coordinate2D(11, 7));
 
-        for (var c : List.of(a, middle, b)) {
-            assertTrue(line1.contains(c));
-            assertTrue(line2.contains(c));
-        }
+        assertFalse(squareLine.hasSinglePointInsideLineIntersectionWith(edgeLine));
     }
 
     @Test
